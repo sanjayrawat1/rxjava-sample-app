6e9a37c8b0816dd4c7fc0efb9d845206a22636c0
Sanjay Singh Rawat <s.rawat1339@gmail.com>
2019-08-30 23:59:11 +0530

    Initial commit

---
+++ src/main/java/io/github/sanjayrawat1/reactivex/RxjavaSampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.sanjayrawat1.reactivex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxjavaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxjavaSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sanjayrawat1/reactivex/RxjavaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sanjayrawat1.reactivex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxjavaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
